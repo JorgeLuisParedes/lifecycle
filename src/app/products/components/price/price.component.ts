ae7ec658af3bcbcec606d88bd28015bc10b66fbd
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-18 13:21:08 +0200

    ngOnDestroy

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges, Input } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
 	selector: 'products-price',
@@ -9,8 +10,11 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
 	@Input()
 	public price: number = 0;
 
+	public interval$?: Subscription;
+
 	ngOnInit(): void {
 		console.log('Componente HIJO: ngOnInit');
+		this.interval$ = interval(1000).subscribe((value) => console.log(`Tick: ${value}`));
 	}
 	ngOnChanges(changes: SimpleChanges): void {
 		console.log({ changes });
@@ -18,5 +22,6 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
 	}
 	ngOnDestroy(): void {
 		console.log('Componente HIJO: ngOnDestroy');
+		this.interval$?.unsubscribe();
 	}
 }
